d1d675366ada85796c1f0c06e7d83b3eabb8c3c4
oluwasemilorebadejo <badejo.semilore.7@gmail.com>
2025-09-16 18:10:54 +0100

    navbar fix

--- src/components/TopBanner.tsx
+++ src/components/TopBanner.tsx
@@ -3,8 +3,8 @@ import { ArrowUpRight } from "lucide-react";
 
 const TopBanner = () => {
   return (
-    <div className="fixed top-0 left-0 right-0 z-[9999] bg-white text-black py-3 px-4 sm:px-6 md:px-8">
-      <div className="max-w-7xl mx-auto flex flex-row flex-wrap items-center justify-center gap-2 text-base">
+    <div className="fixed top-0 left-0 right-0 z-[9999] bg-white text-black py-3 px-1 sm:px-6 md:px-8">
+      <div className="max-[430px]:text-[10px] max-w-7xl md:mx-auto flex flex-row flex-wrap items-center justify-center gap-2  sm:text-sm md:text-base">
         {/* Series A Announcement */}
         <div className="flex items-center gap-1">
           <span>DYNA just raised</span>
