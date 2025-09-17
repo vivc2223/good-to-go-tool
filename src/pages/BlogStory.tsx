06f0df2ebb7cb4f3db7d3b6f247e957a3931a77d
oluwasemilorebadejo <badejo.semilore.7@gmail.com>
2025-09-17 10:38:34 +0100

    add rounded borders to founders image

--- src/pages/BlogStory.tsx
+++ src/pages/BlogStory.tsx
@@ -382,7 +382,7 @@ const BlogStory = () => {
             <img
               src="/lovable-uploads/founders.jpg"
               alt="DYNA Founders"
-              className="max-w-lg h-auto"
+              className="max-w-lg h-auto rounded-lg"
             />
           </div>
         </section>
