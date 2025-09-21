e2687aa6a040a034804bf605b07fc4fadbf072a9
oluwasemilorebadejo <badejo.semilore.7@gmail.com>
2025-09-21 21:26:47 +0100

    new research image

--- src/pages/Research.tsx
+++ src/pages/Research.tsx
@@ -255,7 +255,7 @@ const Research = () => {
                     }}
                   >
                     <img
-                      src="/lovable-uploads/researchh.png"
+                      src="/lovable-uploads/research.png"
                       alt="DYNA Robot Research"
                       className="w-full h-[650px] transition-transform duration-500 group-hover:scale-105"
                       style={{
@@ -497,7 +497,7 @@ const Research = () => {
                   }}
                 >
                   <img
-                    src="/lovable-uploads/researchh.png"
+                    src="/lovable-uploads/research.png"
                     alt="DYNA Robot Research"
                     className="w-full h-full object-contain transition-transform duration-500 group-hover:scale-105"
                     fetchPriority="high"
